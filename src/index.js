d9a484a86f085d89783c095e581e27d26a794426
ctvidic <chrisvidic.cv@gmail.com>
2021-08-14 13:02:17 -0400

    snowpack

--- src/index.js
+++ src/index.js
@@ -1,43 +1,68 @@
 import "./styles/index.scss"
 
 document.addEventListener("DOMContentLoaded", () =>{
-    class displayCanvas{
+    class DisplayMountain{
         constructor(){
             this.canvas = document.getElementById("display-canvas")
             this.ctx = this.canvas.getContext('2d');
             let slopeSlider = document.getElementById("slope");
             let slopeValue = slopeSlider.value;
-            this.slopeVal = slopeValue;
+            this.slopeVal = Number.parseInt(slopeValue);
             this.drawMountain();
-            this.animate = this.animate.bind(this);
         }
 
         drawMountain(){
             let peak = this.canvas.height / 3;
             this.ctx.beginPath();
-            this.ctx.moveTo(0, this.canvas.height);
-            this.ctx.lineTo(this.canvas.width / 2, peak);
-            this.ctx.lineTo(this.slopeVal/90*this.canvas.width, this.canvas.height);
+            this.ctx.moveTo(0, 2/3*this.canvas.height);
+            this.ctx.lineTo(this.canvas.width / 3, peak);
+            this.ctx.lineTo(-4*this.slopeVal+this.canvas.width, this.canvas.height);
             this.ctx.stroke();
         }
+      
+    }
 
+    class DisplaySnow{
+        constructor(slopeVal) {
+            this.canvas = document.getElementById("display-canvas")
+            this.ctx = this.canvas.getContext('2d');
+            let snowSlider = document.getElementById("snow");
+            let snowValue = snowSlider.value;
+            this.snowVal = Number.parseInt(snowValue);
+            this.slopeVal = slopeVal
+            this.drawSnow();
+        }
+
+        drawSnow() {
+            let peak = this.canvas.height / 3;
+            this.ctx.beginPath();
+            this.ctx.moveTo(0, 2 / 3 * this.canvas.height - this.snowVal);
+            debugger;
+            this.ctx.lineTo(this.canvas.width / 3, peak - this.snowVal);
+            this.ctx.lineTo(-4 * this.slopeVal + this.canvas.width, this.canvas.height - this.snowVal);
+            this.ctx.stroke();
+        }
+    }
+
+    class DisplayCanvas {
+        constructor(){
+            this.canvas = document.getElementById("display-canvas")
+            this.ctx = this.canvas.getContext('2d');
+            this.animate = this.animate.bind(this)
+
+        }
         animate(){
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-            // this.drawMountain();
-            let mountainCanvas = new displayCanvas;
+            let mountainCanvas = new DisplayMountain;
+            let snowCanvas = new DisplaySnow(mountainCanvas.slopeVal);
             requestAnimationFrame(this.animate)
         }
-        
-
-
     }
     
     
+    let displayCanvas = new DisplayCanvas;
+    displayCanvas.animate();
+    // let mountainCanvas = new displayMountain;
+    // mountainCanvas.animate();
 
-    let mountainCanvas = new displayCanvas;
-    mountainCanvas.animate();
-    
-
-
-  
 });

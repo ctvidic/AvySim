c5a4eef5526ade271acbdcf0ba24471d751caa05
ctvidic <chrisvidic.cv@gmail.com>
2021-08-14 23:06:51 -0400

    photo

--- src/index.js
+++ src/index.js
@@ -39,6 +39,8 @@ document.addEventListener("DOMContentLoaded", () =>{
             let peak = this.canvas.height / 3;
             this.ctx.beginPath();
             this.ctx.moveTo(0, 2 / 3 * this.canvas.height - this.snowVal);
+            
+            //Slope Description
             if (this.slopeVal > 70 && this.slopeVal <= 75) {
                 this.snowVal /= 1.5;
                 outofrange.style.display = "block";
@@ -60,7 +62,9 @@ document.addEventListener("DOMContentLoaded", () =>{
             }
             this.ctx.lineTo(this.canvas.width / 5, peak - this.snowVal);
             this.ctx.lineTo(-6 * this.slopeVal + this.canvas.width, this.canvas.height - this.snowVal);
+            this.ctx.lineTo(-6 * this.slopeVal + this.canvas.width,this.canvas.height)
             this.ctx.stroke();
+            
         }
     }
 
@@ -68,8 +72,7 @@ document.addEventListener("DOMContentLoaded", () =>{
         constructor(){
             this.canvas = document.getElementById("display-canvas")
             this.ctx = this.canvas.getContext('2d');
-            let windSlider = document.getElementById("windspeed");
-            this.windValue = Number.parseInt(windSlider.value);
+         
             this.windArray = [];
             this.x=0;
             this.y=0
@@ -79,31 +82,35 @@ document.addEventListener("DOMContentLoaded", () =>{
         }
 
         drawWind(){
-            // let peak = this.canvas.height / 3;
             let windSlider = document.getElementById("windspeed");
             this.windValue = Number.parseInt(windSlider.value);
-            // for(let i =0;i<this.windValue;i++){
+            // let peak = this.canvas.height / 3;
+            // for(let i =0;i<(this.windValue/2);i++){
+            //     let y = Math.random()*100;
             //     this.ctx.beginPath();
-            //     this.ctx.moveTo(this.canvas.width / 5+this.x, peak+this.gravity-this.rotation*Math.random());
-            //     this.ctx.lineTo(this.canvas.width / 5 + this.windValue*10+this.x, Math.random()*(20) + peak-50+this.gravity+this.rotation)
+            //     this.ctx.moveTo(this.x,y);
+            //     this.ctx.lineTo(this.windValue*5 + this.x, y)
             //     this.ctx.stroke();
             // }
             // this.x+=this.windValue/10;
-            // if (this.x > 700){
+            if (this.windValue > 10){
+                let y = Math.random() * 4-7
+                this.ctx.moveTo(0, 50);
+                this.ctx.lineTo(150 + this.windValue / 2, 50 + y * this.windValue / 20);
+                this.ctx.moveTo(0, 100);
+                this.ctx.lineTo(100 + this.windValue / 2, 100+y*this.windValue/20);
+                this.ctx.moveTo(0, 150);
+                this.ctx.lineTo(50+this.windValue/1.5, 150+y*this.windValue/20);
+                this.ctx.moveTo(0, 200);
+                this.ctx.lineTo(this.windValue, 200+y*this.windValue/20);
+                this.ctx.stroke();
+            }
+            // this.x += this.windValue/3
+
+            // if (this.windValue * 5 + this.x > 1160){
             //     this.x = 0;
             // }
-            // if (this.y > this.canvas.height) {
-            //     this.y = 0;
-            // }
-            // this.gravity+=2;
-            // this.rotation+=1;
-            // this.y+=this.gravity;
-            debugger;
-            if (this.windValue > 10){
-                windloaded.style.display = "block";
-            }else{
-                windloaded.style.display = "none";
-            }
+
         }
     }
 
@@ -112,18 +119,70 @@ document.addEventListener("DOMContentLoaded", () =>{
         
     }
 
+    class DisplayTemperature{
+        constructor(){
+            this.canvas = document.getElementById("display-canvas")
+            this.ctx = this.canvas.getContext('2d');
+            this.drawTemp();
+        }
+        drawTemp(){   
+        }
+    }
+
+    class TextBox{
+        constructor(){
+            this.canvas = document.getElementById("display-canvas")
+            this.ctx = this.canvas.getContext('2d');
+            
+            this.createText();
+        }
+
+        createText(){
+            let tempSlider = document.getElementById("temperature");
+            let tempValue = Number.parseInt(tempSlider.value);
+            let windSlider = document.getElementById("windspeed");
+            let windValue = Number.parseInt(windSlider.value);
+            let snowSlider = document.getElementById("snow");
+            let snowValue = Number.parseInt(snowSlider.value);
+
+            if (snowValue < 10) {
+                lowsnow.style.display = "block";
+                wet.style.display = "none";
+                windloaded.style.display = "none";
+            } else {
+                lowsnow.style.display = "none"
+                if (tempValue > 40) {
+                    wet.style.display = "flex"
+                } else {
+                    wet.style.display = "none"
+                }
+
+                if (windValue > 30 && tempValue < 40) {
+                    windloaded.style.display = "block";
+                } else {
+                    windloaded.style.display = "none";
+                }
+            }
+        }
+
+    }
+
     class DisplayCanvas {
         constructor(){
             this.canvas = document.getElementById("display-canvas")
             this.ctx = this.canvas.getContext('2d');
             this.animate = this.animate.bind(this)
             this.windCanvas = new DisplayWind;
+            this.tempCanvas = new DisplayTemperature;
+            this.textbox = new TextBox;
         }
         animate(){
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
             let mountainCanvas = new DisplayMountain;
             let snowCanvas = new DisplaySnow(mountainCanvas.slopeVal);
             this.windCanvas.drawWind();
+            this.tempCanvas.drawTemp();
+            this.textbox.createText();
             requestAnimationFrame(this.animate)
         }
     }
